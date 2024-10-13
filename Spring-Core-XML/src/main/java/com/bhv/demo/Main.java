bd838de05e6bb05809d364c2bc9f15063ba4c9c5
bhavik <bhavikvashi1804@gmail.com>
2024-10-13 21:00:17 +0530

    Spring Core: Bean Life Cycle Method: XML Config: Destroy

--- Spring-Core-XML/src/main/java/com/bhv/demo/Main.java
+++ Spring-Core-XML/src/main/java/com/bhv/demo/Main.java
@@ -1,14 +1,15 @@
 package com.bhv.demo;
 
 import com.bhv.demo.lifecycle.Home;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Home home= applicationContext.getBean("home", Home.class);
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Home home= context.getBean("home", Home.class);
         System.out.println(home);
+        context.registerShutdownHook();
     }
 }
