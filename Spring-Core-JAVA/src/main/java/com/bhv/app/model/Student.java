02fa462350a667b96c8fc2b686d9f0973c835967
bhavik <bhavikvashi1804@gmail.com>
2024-11-04 20:08:53 +0530

    Spring Core: Annotation based config & @Autowired

--- Spring-Core-JAVA/src/main/java/com/bhv/app/model/Student.java
+++ Spring-Core-JAVA/src/main/java/com/bhv/app/model/Student.java
@@ -1,5 +1,6 @@
 package com.bhv.app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +8,7 @@ public class Student {
 
     private String name = "Stephen Markins";
 
+    @Autowired
     private School school;
 
     @Override
