fdf8f6d7849420ed41321b1f6c5f2f7d9571d4b8
bhavik <bhavikvashi1804@gmail.com>
2024-11-04 19:56:32 +0530

    Spring Core: Auto Wiring & @Primary Annotation

--- Spring-Core-JAVA/src/main/java/com/bhv/app/config/AppConfig.java
+++ Spring-Core-JAVA/src/main/java/com/bhv/app/config/AppConfig.java
@@ -5,17 +5,34 @@ import com.bhv.app.model.Person;
 import com.bhv.app.model.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 
 
-    @Bean
+    @Bean(name = "vehicle")
     Vehicle vehicle() {
         Vehicle vehicle = new Vehicle();
         vehicle.setName("BMW M5");
         return vehicle;
     }
+
+    @Bean
+    Vehicle vehicle1() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("BMW M3");
+        return vehicle;
+    }
+
+    @Bean
+    @Primary
+    Vehicle vehicle2() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Astin Martin DB12");
+        return vehicle;
+    }
+
 
     @Bean
     Person person(Vehicle vehicle) {
